242e77a007d20acc743a9783175961595016b9b0
amriksheer <amriks.heer@gmail.com>
2021-05-02 10:21:54 +0530

    2 files modified, 26 added, 26 deleted

--- src/index.js
+++ src/index.js
@@ -5,5 +5,19 @@ import App from "./App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
-fgg;
-h
+const { of, timer } = require("rxjs");
+const { concatAll, mergeAll, take, delay } = require("rxjs/operators");
+
+const source1 = timer(0, 1).pipe(take(10));
+
+const workingDays = ["Monday", "Tuesday", "Wednesday", "Thurday", "Friday"];
+const source2 = of(...workingDays).pipe(delay(15));
+
+const weekends = ["Saturday", "Sunday"];
+const source3 = of(...weekends).pipe(delay(35));
+
+
+console.log("# merge working days and weekends");
+of(source1, source2, source3)
+  .pipe(concatAll(1))
+  .subscribe(x => console.log(x));

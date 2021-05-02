419778dcc708d27e02f8ae148903aab5010c4a31
amriksheer <amriks.heer@gmail.com>
2021-05-02 10:35:40 +0530

    1 file modified

--- src/index.js
+++ src/index.js
@@ -5,19 +5,11 @@ import App from "./App";
 
 ReactDOM.render(<App />, document.getElementById("root"));
 
-const { of, timer } = require("rxjs");
-const { concatAll, mergeAll, take, delay } = require("rxjs/operators");
+const { of } = require("rxjs");
+const { reduce } = require("rxjs/operators");
 
-const source1 = timer(0, 1).pipe(take(10));
-
-const workingDays = ["Monday", "Tuesday", "Wednesday", "Thurday", "Friday"];
-const source2 = of(...workingDays).pipe(delay(15));
-
-const weekends = ["Saturday", "Sunday"];
-const source3 = of(...weekends).pipe(delay(35));
-
-
-console.log("# merge working days and weekends");
-of(source1, source2, source3)
-  .pipe(concatAll(1))
+console.log("# sum of sequence");
+console.log("# like scan, but only emits the final value");
+of(1, 2, 3)
+  .pipe(reduce((acc, val) => acc + val, 0))
   .subscribe(x => console.log(x));
